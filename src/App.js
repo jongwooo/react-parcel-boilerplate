92e09791a75f4102eff04fd79ae08543913b2a9e
jongwooo <han980817@gmail.com>
2022-07-22 11:58:55 +0900

    feat: Create components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import './App.css'
+import Logo from './logo.svg'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <Logo className="App-logo" />
+    </div>
+  )
+}
+
+export default App
